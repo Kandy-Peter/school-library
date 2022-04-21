350ad0db2ea01876e8388b394436cb9db44f3956
kandy-peter <kandypeter03@gmail.com>
2022-04-21 20:23:58 +0200

    implemnts relationshipt between classes

--- src/student.rb
+++ src/student.rb
@@ -1,14 +1,19 @@
 require_relative './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: false)
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
-  belongs_to :classroom
-
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.student.include?(self)
   end
 end
